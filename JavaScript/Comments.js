1eef202da48ba84aef6f1130755bbed286163dfd
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-22 15:17:10 -0400

    comments

---
+++ JavaScript/Comments.js
@@ -0,0 +1,6 @@
+/* Comments */
+// Comments are like a manual to our code and the compiler doesn't read them.
+let name = 'Jack';
+let age = 23;
+message = "My name is " + name +",and I am" + age + "years old";
+console.log(message);
