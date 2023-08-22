f6052158ceba93df3a2360d5e393df0b80b1c324
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-22 15:31:50 -0400

    Variables and Strings

---
+++ JavaScript/variableAndStrings.js
@@ -0,0 +1,12 @@
+/* Variables */
+// Variables are like boxes where we can put something in our case a data.
+// Variables can have different types such as strings, numbers, floats...
+// And string are values which will be inside "" or ''
+let name = 'Yeabsira';
+let age = 24;
+let dateOfBirth = "10-09-82";
+let linkedInProfile = "www.linkedin.com/in/yeabsira-mesfin-3b211b234"
+// The following is called concatenation.
+let message = "My name is " + name +",and I am" + age + "years old. " + + " I was born in " + dateOfBirth + "you can see my linkedIn prifile using this link " + linkedInProfile;
+
+console.log(message);
