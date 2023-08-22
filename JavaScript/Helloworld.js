2cd298871dc84f4af9df314fb0568ec44dd015fa
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-22 15:10:44 -0400

    String

--- JavaScript/Helloworld.js
+++ JavaScript/Helloworld.js
@@ -1,5 +1,10 @@
-let name = 'Yeabsira';
-let age =24;
-let message = "Hi, my name is " + name + " and I am " + age + " years old";
-console.log(message)
-alert(message)
+// let name = 'Yeabsira';
+// let age =24;
+// let message = "Hi, my name is " + name + " and I am " + age + " years old";
+// console.log(message)
+// alert(message)
+
+/* String */
+let val = 'Hey';
+let val2 = 'boss';
+console.log(val,val2)
