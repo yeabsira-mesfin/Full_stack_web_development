916e84dd38aa27945ed699536a5ad3e6b0d43553
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-22 15:51:25 -0400

    numbers

---
+++ JavaScript/Numbers.js
@@ -0,0 +1,15 @@
+/* We have different kinds of numbers in JavaScript such as float, decimal.... */
+// We can perform different kinds of operations such as addition, mulitplication and division.
+// And we have different functions such as Math using numbers in JavaScript.
+let sum = 10 + 6;
+let sub = 20 -6;
+let mul = 4 * 4;
+let div = 32/2;
+let mod =  32 % 3;
+let allInOne = (10 * 6 / 2 - 50 + 100 - 48)/ 2 ;
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(mod)
+console.log(allInOne)
