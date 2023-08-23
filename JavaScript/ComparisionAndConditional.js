ef5ead24d3e441cbfe2cb775558c1856027628f3
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-23 11:14:56 +0300

    Camparision and Conditional operators

---
+++ JavaScript/ComparisionAndConditional.js
@@ -0,0 +1,33 @@
+/* Comparision and Conditional Operators. */
+// Comparision operators compare and return values. (<, > , ==, ===, <=,>=,!=)
+// Whereas the conditional operators execute based on certain criterias.(if ...,else if ... ,if ... else)
+let myAccountBalance = 300;
+let expensiveShoe = 694.93;
+let coupon = 400;
+
+if(expensiveShoe <= myAccountBalance){
+    myAccountBalance -= expensiveShoe
+    console.log("You bought a shoe.");
+    console.log('Your account balance is ',myAccountBalance)
+}
+else if(expensiveShoe - coupon <= expensiveShoe){
+    myAccountBalance -= expensiveShoe - coupon
+    console.log("You bought a shoe.");
+    console.log('Your account balance is ',myAccountBalance)
+}
+else{
+    console.log('Your acount balance is insufficient')
+}
+
+let age = 23;
+let yourage = '23';
+
+// if(age == yourage){
+//     console.log("We have the same age.")
+// }
+if(age === yourage){
+    console.log("we have same data types.")
+}
+else {
+    console.log("We have different data types.")
+}
