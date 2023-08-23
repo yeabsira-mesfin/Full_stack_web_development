5d0a58a691a6947bd602b12018f1be5ade14fb1d
yeabsira_mesfin <yabidesta@gmail.com>
2023-08-23 11:17:26 +0300

    including not equal to

--- JavaScript/ComparisionAndConditional.js
+++ JavaScript/ComparisionAndConditional.js
@@ -31,3 +31,9 @@ if(age === yourage){
 else {
     console.log("We have different data types.")
 }
+
+let num1 = 5;
+let num2 = 8;
+if(num1 != num2){
+    console.log('The numbers are not equal.')
+}
