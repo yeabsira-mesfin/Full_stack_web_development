dd09a1317aa46aa7abfc38a6dbe170be614995c9
yeabsira_mesfin <yabidesta@gmail.com>
2023-09-20 13:54:07 +0300

    Post

--- Node/FirstApI/README.md
+++ Node/FirstApI/README.md
@@ -5,3 +5,4 @@
 # Then a JSON file will appear.
 # After the JSON file appears we need to use the following instruction
 # npm install --save express (Expresss is used to create web request(handles web requests and know what it does with them.))
+# npm install --save body-parser, this instruction allow us to send and receive JSON and manage nested objects.
--- Node/FirstApI/server.js
+++ Node/FirstApI/server.js
@@ -1,25 +1,45 @@
-/* This code creates a simple web server using Node.js and Express.js. It has two routes: one for the root path ('/') and another for '/yeabyy'. 
-When you access these routes, the server sends back corresponding text responses to the client. You can start the server by running this script,
-and it will be accessible at http://localhost:3000/ and http://localhost:3000/yeabyy in your web browser.*/
-
-/*   This line imports the Express.js framework, which simplifies building web applications in Node.js. */
 let express = require('express');
-
-/* Here, an instance of the Express application is created. This app variable is used to configure and define routes for your web application */
 let app = express();
+let bodyParser = require('body-parser');
 
-/* This code sets up a route for the root URL path ('/'). When a user accesses the root URL of your web server, the function provided in the
-second argument will be executed. In this case, it sends the string 'My first API' as a response to the client. */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+let ingredients = [
+    {
+        "id":"232kAK",
+        "text":"Egg"
+    },
+    {
+        "id":"dkP345",
+        "text":"Chicken"
+    }, 
+    {
+        "id":"dkcuu7",
+        "text":"Beef"
+    },
+    {
+        "id":"73hdy",
+        "text":"Cheese"
+    }
+];
 app.get('/',function(request,response) {
-    response.send('My first API');   
+    response.send(ingredients);   
 })
 
-/*   This code sets up a route for the path '/yeabyy'. When a user accesses this URL, the function provided will send the string "Yeabby you're progressing" as a response. */
-app.get('/yeabyy',function(request,response){
-    response.send("Yeabby you're progressing")
+app.post('/',function(request,response){
+    let ingredient = request.body
+    if(!ingredient || ingredient.text === ""){
+        response.status(500).send({error: "Your ingredient must have at least one"});
+    }
+    else{
+        ingredients.push(ingredient);
+        response.status(200).send(ingredients);
+    }
 })
+// app.get('/yeabyy',function(request,response){
+//     response.send("Yeabby you're progressing")
+// })
 
-/* This line tells Express to start listening on port 3000. When the server starts, it prints the message "First API running on port 3000!" to the console. */
 app.listen(3000,function(){
     console.log("First API running on port 3000!")
 })
